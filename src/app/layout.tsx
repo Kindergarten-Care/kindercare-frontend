a97798279ff68806d9c0cd9c180929b0a5b8cd9b
Danny Ho <hocong.danh16@gmail.com>
2026-04-16 21:51:26 +0700

    add metadata tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,20 @@
 import StyledComponentsRegistry from '@/lib/registry';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'KinderCare - Hệ Thống Quản Lý Mầm Non',
+  description: 'Nền tảng quản lý mầm non toàn diện, hiện đại và bảo mật.',
+  icons: {
+    icon: '/favicon.ico', // Bạn có thể thay đổi icon tại đây
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
+    <html lang="vi">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
