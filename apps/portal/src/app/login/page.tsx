0e47ede1e1e90e64cdd219103a911bdc0e20706a
Nguyen Minh Chanh <nguyenminhchanh27@gmail.com>
2026-05-28 23:54:50 +0700

    revert: restore LoginPage and ForgotPasswordPage back to original SSR imports to recover from production blank screen

--- apps/portal/src/app/login/page.tsx
+++ apps/portal/src/app/login/page.tsx
@@ -1,13 +1,9 @@
 'use client';
 
 import React from 'react';
-import dynamic from 'next/dynamic';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '@kindercare/ui';
-
-const LoginView = dynamic(() => import('../../views/Login'), {
-  ssr: false,
-});
+import { LoginView } from '@/views/Login';
 
 export default function LoginPage() {
   return (
